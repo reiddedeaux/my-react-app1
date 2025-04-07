8cd4f913a57284f7a4a52bb7a0dbbcef8fd6bad3
reiddedeaux <rdedeaux@usf.edu>
2025-04-06 20:17:58 -0400

    Initial commit

---
+++ src/Components/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greetings() {
+    return (
+<div>
+    <h2> Hello from the Greeting Component!</h2>
+    <p>This is your first custom component in React.</p>
+</div>
+    );
+}
+export default Greetings;
